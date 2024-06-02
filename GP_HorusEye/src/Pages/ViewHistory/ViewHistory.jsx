f977d3c08b70a6cbb352b075865971707ef20f0f
Amir202000160 <amirsamir225@gmail.com>
2024-06-02 14:54:08 +0300

    ViewHistory
    Viewing history for booked reservations and landmarks that get into the HorusEye (AI model)

--- GP_HorusEye/src/Pages/ViewHistory/ViewHistory.jsx
+++ GP_HorusEye/src/Pages/ViewHistory/ViewHistory.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './ViewHistory.css'
 import { useRecoilValue } from 'recoil';
 import { CityState,CheckInState,CheckOutState, RoomState } from '../Booking/Booking';
+import axios from 'axios';
 
 function ViewHistory() {
 
-const [BookedHistory, SetBookedHistory] =useState(false)
+const [History, SetHistory] =useState([])
+const [BookedHistory, SetBookedHistory] =useState(true)
 const [LandMarked, SetLandMarked] =useState(false)
 let city =useRecoilValue(CityState)
 let checkIn =useRecoilValue(CheckInState)
 let checkOut =useRecoilValue(CheckOutState)
 let room =useRecoilValue(RoomState)
+
+useEffect(() => {
+  axios.get('http://localhost:3000/product')
+  .then((response) => {
+    SetHistory(response.data)
+    console.log(response.data)
+  }).catch
+  ((error) => {
+    console.log(error)
+  })
+}, [])
+  
+
+
+
 
 
 const Booked=()=>{
@@ -30,23 +47,19 @@ const LandMark=()=>{
       <a className='Navigate' onClick={LandMark}>Landmarks</a>
       </div>
       <div className='Line'></div>
-      {
-      !BookedHistory && !LandMarked && (<h1>there is no data</h1>)
-      }
-    { BookedHistory && (<div className='History'>
-        <div className='HistoryCard'>
-          <div className='HistoryCard1'>
-            <h2>Hotel name:{} </h2>
-            <p>City:{city} </p>
-            <p>CheckIn: {checkIn}</p>
-            <p>CheckOut: {checkOut}</p>
-            <p>Room: {room}</p>
-            <p>Price/Night: </p>
-            <p>Rating: </p>
-            <p>Description: </p>
-          </div>
+      
+    { BookedHistory && (History.map((history) =>{
+      return(
+        <div className='History 'key={history.id}>
+              <h2 >Hotel name: </h2>
+              <p>City: </p>
+              <p>CheckIn: {history.checkIn}</p>
+              <p>CheckOut: {history.checkOut}</p>
+              <p>Room: {history.room}</p>
+
         </div>
-      </div>) }
+      )
+    }) ) }
       { LandMarked && (<div className='History'>
         <div className='HistoryCard'>
           <div className='HistoryCard1'>
