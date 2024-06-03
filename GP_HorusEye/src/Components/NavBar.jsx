77fcfa4e7b9ea4e152b80f5eadae2edc7980d718
FADY <ofady405@gmail.com>
2024-06-03 15:22:10 +0300

    navigationbar

--- GP_HorusEye/src/Components/NavBar.jsx
+++ GP_HorusEye/src/Components/NavBar.jsx
@@ -10,29 +10,25 @@ function NavBar(){
        /* <div className="navbar bg-body-tertiary MainNav p-0 position-fixed ">*/
           <div className="container-fluid  NAV ">
 
-            <Link to="/" className="navbar-brand IMG" href="#"  alt="Bootstrap" width="30" height="24"> 
+            <Link to="/" className="navbar-brand IMG" href="#"  alt="Bootstrap" width="25" height="25"> 
             <img src='/public/Group 4.png' />
             </Link>
-            
+            <div className='middle_elements'>
             <Link to='/HorusEye' className="navbar-brand NAVi" href="#" > 
-            <h3>Horus eye</h3>
+            <p className='NAVi'>Horus eye</p>
             </Link>
             <Link to='/Booking' className="navbar-brand NAVi" href="#" > 
-            <h3>Booking</h3>
+            <p className='NAVi'>Booking</p>
+            </Link>
+            <Link to ="/FAV" className="navbar-brand NAVi" href="#" >
+              <p className='NAVi'> Favourites</p></Link>
+            <Link to="/ViewHistory" className="navbar-brand NAVi " href="#"  > 
+            <p className='NAVi'>History</p>
             </Link>
             <Link to="/Login" className="navbar-brand NAVi " href="#" > 
-           <h3>Login</h3>
+            <p className='NAVi'>Login</p>
             </Link>
-            <Link to ="/FAV" className="navbar-brand NAVi" href="#" > 
-            <h3>FAV</h3>
-            </Link>
-           {/*<a className="navbar-brand NAVi " href="#" > 
-            <h3>Menu</h3>
-    </a>*/}
-            <Link to="/ViewHistory" className="navbar-brand NAVi " href="#" > 
-            <h3>View History</h3>
-            </Link>
-
+            </div>
           </div>
       /*  </div>*/
         
